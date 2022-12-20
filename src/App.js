4a1285cee21f5b558d0408f62467f9286d0a84cb
rashedadwan99 <rashedadwan56789@gmail.com>
2022-12-20 12:18:49 +0300

    solving the problem of not found

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import ChatPage from "./pages/ChatPage";
 import HomePage from "./pages/HomePage";
 import "./App.css";
@@ -7,16 +7,15 @@ import "./App.css";
 
 
 function App() {
-  const user = localStorage.getItem("user")
 
   return (
     <div className="App">
-      <Switch>
 
-        <Route path="/" component={HomePage} exact />
-        <Route path="/chats" component={ChatPage} />
 
-      </Switch>
+      <Route path="/" component={HomePage} exact />
+      <Route path="/chats" component={ChatPage} />
+
+
     </div>
   );
 }
